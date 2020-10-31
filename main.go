727b6a2b7470a515ea7c3fb685772f1b72e05c8f
Henry Vélez <velezmolina@gmail.com>
2020-10-31 11:58:43 -0500

    Dynamic port
    Signed-off-by: Henry Vélez <velezmolina@gmail.com>

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/julienschmidt/httprouter"
 )
 
-const ServerAddr = ":8080"
+//const ServerAddr = ":8080"
 
 var db Storage
 var router *httprouter.Router
@@ -34,7 +35,22 @@ func welcome(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintf(w, "Welcome to the system")
 }
 
+func getListenPort() (string, error) {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "", fmt.Errorf("$PORT not set")
+
+	}
+	return ":" + port, nil
+
+}
+
 func main() {
-	fmt.Println("The server is on port :8080")
-	log.Fatal(http.ListenAndServe(ServerAddr, router))
+	port, err := getListenPort()
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	fmt.Println("The server is on port " + port)
+	log.Fatal(http.ListenAndServe(port, router))
 }
